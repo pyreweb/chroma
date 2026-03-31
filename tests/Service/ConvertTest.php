a443183f007238efacb59bc63c232ed8b4d3b9e6
Équipe Pyréweb <support@pyreweb.com>
2026-03-31 09:22:14 +0200

    refactor: Renommer les méthodes hex2* en hexTo* pour une cohérence accrue dans les tests de conversion

--- tests/Service/ConvertTest.php
+++ tests/Service/ConvertTest.php
@@ -19,119 +19,119 @@ class ConvertTest extends TestCase
 	{
 		$expected = 'rgb(0, 0, 0)';
 
-		$this->assertSame($expected, Convert::hex2rgb('#000000'));
-		$this->assertSame($expected, Convert::hex2rgb(Color::Black->getHex()));
+		$this->assertSame($expected, Convert::hexToRgb('#000000'));
+		$this->assertSame($expected, Convert::hexToRgb(Color::Black->getHex()));
 	}
 
 	public function testHex2RgbWhite(): void
 	{
 		$expected = 'rgb(255, 255, 255)';
 
-		$this->assertSame($expected, Convert::hex2rgb('#FFFFFF'));
-		$this->assertSame($expected, Convert::hex2rgb(Color::White->getHex()));
+		$this->assertSame($expected, Convert::hexToRgb('#FFFFFF'));
+		$this->assertSame($expected, Convert::hexToRgb(Color::White->getHex()));
 	}
 
 	public function testHex2RgbRed500(): void
 	{
 		$expected = 'rgb(239, 68, 68)';
 
-		$this->assertSame($expected, Convert::hex2rgb('#ef4444'));
-		$this->assertSame($expected, Convert::hex2rgb(Color::Red500->getHex()));
+		$this->assertSame($expected, Convert::hexToRgb('#ef4444'));
+		$this->assertSame($expected, Convert::hexToRgb(Color::Red500->getHex()));
 	}
 
 	public function testHex2RgbaBlack(): void
 	{
 		$expected = 'rgba(0, 0, 0, 1)';
 
-		$this->assertSame($expected, Convert::hex2rgba('#000000'));
-		$this->assertSame($expected, Convert::hex2rgba(Color::Black->getHex()));
+		$this->assertSame($expected, Convert::hexToRgba('#000000'));
+		$this->assertSame($expected, Convert::hexToRgba(Color::Black->getHex()));
 	}
 
 	public function testHex2RgbaWhite(): void
 	{
 		$expected = 'rgba(255, 255, 255, 1)';
 
-		$this->assertSame($expected, Convert::hex2rgba('#FFFFFF'));
-		$this->assertSame($expected, Convert::hex2rgba(Color::White->getHex()));
+		$this->assertSame($expected, Convert::hexToRgba('#FFFFFF'));
+		$this->assertSame($expected, Convert::hexToRgba(Color::White->getHex()));
 	}
 
 	public function testHex2RgbaRed500(): void
 	{
 		$expected = 'rgba(239, 68, 68, 1)';
 
-		$this->assertSame($expected, Convert::hex2rgba('#ef4444'));
-		$this->assertSame($expected, Convert::hex2rgba(Color::Red500->getHex()));
+		$this->assertSame($expected, Convert::hexToRgba('#ef4444'));
+		$this->assertSame($expected, Convert::hexToRgba(Color::Red500->getHex()));
 	}
 
 	public function testHex2HslBlack(): void
 	{
 		$expected = 'hsl(0, 0%, 0%)';
 
-		$this->assertSame($expected, Convert::hex2hsl('#000000'));
-		$this->assertSame($expected, Convert::hex2hsl(Color::Black->getHex()));
+		$this->assertSame($expected, Convert::hexToHsl('#000000'));
+		$this->assertSame($expected, Convert::hexToHsl(Color::Black->getHex()));
 	}
 
 	public function testHex2HslWhite(): void
 	{
 		$expected = 'hsl(0, 0%, 100%)';
 
-		$this->assertSame($expected, Convert::hex2hsl('#FFFFFF'));
-		$this->assertSame($expected, Convert::hex2hsl(Color::White->getHex()));
+		$this->assertSame($expected, Convert::hexToHsl('#FFFFFF'));
+		$this->assertSame($expected, Convert::hexToHsl(Color::White->getHex()));
 	}
 
 	public function testHex2HslRed500(): void
 	{
 		$expected = 'hsl(0, 84.24%, 60.2%)';
 
-		$this->assertSame($expected, Convert::hex2hsl('#ef4444'));
-		$this->assertSame($expected, Convert::hex2hsl(Color::Red500->getHex()));
+		$this->assertSame($expected, Convert::hexToHsl('#ef4444'));
+		$this->assertSame($expected, Convert::hexToHsl(Color::Red500->getHex()));
 	}
 
 	public function testHex2OklchBlack(): void
 	{
 		$expected = 'oklch(0%, 0%, 0)';
 
-		$this->assertSame($expected, Convert::hex2oklch('#000000'));
-		$this->assertSame($expected, Convert::hex2oklch(Color::Black->getHex()));
+		$this->assertSame($expected, Convert::hexToOklch('#000000'));
+		$this->assertSame($expected, Convert::hexToOklch(Color::Black->getHex()));
 	}
 
 	public function testHex2OklchWhite(): void
 	{
 		$expected = 'oklch(100%, 0%, 0)';
 
-		$this->assertSame($expected, Convert::hex2oklch('#FFFFFF'));
-		$this->assertSame($expected, Convert::hex2oklch(Color::White->getHex()));
+		$this->assertSame($expected, Convert::hexToOklch('#FFFFFF'));
+		$this->assertSame($expected, Convert::hexToOklch(Color::White->getHex()));
 	}
 
 	public function testHex2OklchRed500(): void
 	{
 		$expected = 'oklch(63.68%, 20.78%, 25.33132777693)';
 
-		$this->assertSame($expected, Convert::hex2oklch('#ef4444'));
-		$this->assertSame($expected, Convert::hex2oklch(Color::Red500->getHex()));
+		$this->assertSame($expected, Convert::hexToOklch('#ef4444'));
+		$this->assertSame($expected, Convert::hexToOklch(Color::Red500->getHex()));
 	}
 
 	public function testHex2CmykBlack(): void
 	{
 		$expected = 'cmyk(0%, 0%, 0%, 100%)';
 
-		$this->assertSame($expected, Convert::hex2cmyk('#000000'));
-		$this->assertSame($expected, Convert::hex2cmyk(Color::Black->getHex()));
+		$this->assertSame($expected, Convert::hexToCmyk('#000000'));
+		$this->assertSame($expected, Convert::hexToCmyk(Color::Black->getHex()));
 	}
 
 	public function testHex2CmykWhite(): void
 	{
 		$expected = 'cmyk(0%, 0%, 0%, 0%)';
 
-		$this->assertSame($expected, Convert::hex2cmyk('#FFFFFF'));
-		$this->assertSame($expected, Convert::hex2cmyk(Color::White->getHex()));
+		$this->assertSame($expected, Convert::hexToCmyk('#FFFFFF'));
+		$this->assertSame($expected, Convert::hexToCmyk(Color::White->getHex()));
 	}
 
 	public function testHex2CmykRed500(): void
 	{
 		$expected = 'cmyk(0%, 71.55%, 71.55%, 6.27%)';
 
-		$this->assertSame($expected, Convert::hex2cmyk('#ef4444'));
-		$this->assertSame($expected, Convert::hex2cmyk(Color::Red500->getHex()));
+		$this->assertSame($expected, Convert::hexToCmyk('#ef4444'));
+		$this->assertSame($expected, Convert::hexToCmyk(Color::Red500->getHex()));
 	}
 }
